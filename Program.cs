74651e273c662a9db142592aa1844ae27d08c3bb
3060s <78622374+3060s@users.noreply.github.com>
2024-01-21 14:25:53 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP_zad_rdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostokat prostokat1 = new Prostokat(2, 4);
+
+            Console.WriteLine($"Pole prostokąta: {prostokat1.Pole()}");
+            Console.WriteLine($"Obwód prostokąta: {prostokat1.Obwod()}");
+
+
+            Trojkat trojkat1 = new Trojkat(3, 4, 5);
+
+            Console.WriteLine($"\nPole trójkąta: {trojkat1.Pole()}");
+            Console.WriteLine($"Obwód trójkąta: {trojkat1.Obwod()}");
+
+
+            Kolo kolo1 = new Kolo(5.27);
+
+            Console.WriteLine($"\nPole koła: {kolo1.Pole()}");
+            Console.WriteLine($"Obwód koła: {kolo1.Obwod()}");
+        }
+    }
+}
---
+++ figura.cs
@@ -0,0 +1,75 @@
+﻿namespace OOP_zad_rdev
+{
+    abstract class Figura
+    {
+        public abstract double Pole();
+        public abstract double Obwod();
+    }
+
+
+    class Prostokat : Figura
+    {
+        private readonly double bok1, bok2;
+
+        public Prostokat(double x, double y)
+        {
+            this.bok1 = x;
+            this.bok2 = y;
+        }
+
+        public override double Pole()
+        {
+            return bok1 * bok2;
+        }
+
+        public override double Obwod()
+        {
+            return (2 * bok1) + (2 * bok2);
+        }
+    }
+
+
+    class Trojkat : Figura
+    {
+        private readonly double bok1, bok2, bok3;
+
+        public Trojkat(double x, double y, double z)
+        {
+            this.bok1 = x;
+            this.bok2 = y;
+            this.bok3 = z;
+        }
+
+        public override double Pole()
+        {
+            double s = (bok1 + bok2  + bok3) / 2;
+            return Math.Sqrt(s * (s - bok1) * (s - bok2) * (s - bok3));
+        }
+
+        public override double Obwod()
+        {
+            return bok1 + bok2+ bok3;
+        }
+    }
+
+
+    class Kolo : Figura
+    {
+        private readonly double promien;
+
+        public Kolo(double r)
+        {
+            this.promien = r;
+        }
+
+        public override double Pole()
+        {
+            return 3.14 * (promien * promien);
+        }
+
+        public override double Obwod()
+        {
+            return 2 * 3.14 * promien;
+        }
+    }
+}
